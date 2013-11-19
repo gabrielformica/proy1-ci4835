901e8e05366d7155d2570277593c08730971a980
Melecio Ponte <pontezambrano@gmail.com>
2013-11-19 19:05:45 -0430

    Creada funcion 'allocate'

--- client.c
+++ client.c
@@ -7,16 +7,27 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 
+void allocate(void *x, int size) {
+  if ((x = malloc(size)) == NULL) {
+    perror("Error malloc");
+    exit(1);
+  }
+}
+
 
 int main(int argc, char *argv[]) {
 	char opts;
+    char *portno;
 	while ((opts = getopt(argc, argv, ":h:p:n:a:")) != -1) {
 		switch (opts) {
 			case 'h': 
 				printf("Flag -h, with parameter %s\n",optarg);
 				break;
-			case 'p': 
-				printf("Flag -p, with paremeter %s\n",optarg);
+			case 'p':
+              allocate(portno,40);
+              portno[3] = 48;
+              
+              printf("Flag -p, with paremeter %d\n", portno[3]);
 				break;
 			case 'n': 
 				printf("Flag -n, with paremeter %s\n",optarg);
