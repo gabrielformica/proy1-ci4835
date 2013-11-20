ed32d6fd68a3ca6113b7e8d85537bb0c6ab2ec3b
gabrielformica <gabrielformica93@gmail.com>
2013-11-19 22:26:24 -0430

    cliente.c intenta conectarse

--- client.c
+++ client.c
@@ -2,10 +2,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h> 
+	
+
+void error(char *msg) {
+	perror(msg);
+	exit(0);
+}
 
 void allocate(void *x, int size) {
   if ((x = malloc(size)) == NULL) {
@@ -16,31 +23,61 @@ void allocate(void *x, int size) {
 
 
 int main(int argc, char *argv[]) {
-	char opts;
-    char *portno;
+
+	int sockfd, portno, charno;
+	char opts, *host, *username, *comfile, buffer[256];
+	struct sockaddr_in serv_addr;	
+	struct hostent *server;
+
 	while ((opts = getopt(argc, argv, ":h:p:n:a:")) != -1) {
 		switch (opts) {
 			case 'h': 
-
-              allocate(portno,40);
-              portno[3] = 23;
-              printf("Flag -h, with parameter %s\n",portno[3]);
+				allocate(host,sizeof(char)*strlen(optarg));
+				host = optarg;
+				printf("Flag -h, with parameter %s\n",host);
 				break;
 			case 'p':
-              allocate(portno,40);
-              portno[3] = 23;
-              
-              
-              printf("Flag -p, with paremeter %d\n", portno[3]);
+				portno = atoi(optarg);
+				printf("Flag -p, with parameter %d\n",portno);
 				break;
 			case 'n': 
-				printf("Flag -n, with paremeter %s\n",optarg);
+				allocate(username,sizeof(char)*strlen(optarg));
+				username = optarg;
+				printf("Flag -n, with paremeter %s\n",username);
 				break;
 			case 'a': 
-				printf("Flag -a, with paremeter %s\n",optarg);
+				allocate(comfile,sizeof(char)*strlen(optarg));
+				comfile = optarg;
+				printf("Flag -a, with paremeter %s\n",comfile);
 				break;
 			case '?':
 				printf("ERROR with flags\n");
 		}
 	}
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+		error("ERROR opening socket");
+
+	if ((server = gethostbyname(host)) == NULL) {
+		error("ERROR no such host");
+	}
+
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr,
+			(char *)&serv_addr.sin_addr.s_addr,
+			server->h_length);
+	serv_addr.sin_port = htons(portno);
+	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) {
+		error("ERROR connecting");
+	}
+	printf("You are connected\n");
+
+/*
+	close(sockfd);
+	free(host);
+	free(comfile); 
+	free(username);
+*/
 }
